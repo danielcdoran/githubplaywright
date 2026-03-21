2f98fdcfa9ab675a39c73de94449cbccffacc8ac
70theridgeway <70theridgeway@gmail.com>
2026-03-21 11:46:36 +0000

    Makes commit in ReadME.ms in zoots

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -24,7 +24,16 @@ test('Create commit from README.md', async ({ page }) => {
   // GitHub shows avatar button only when logged in
 //  await expect(page.getByLabel('Page context').locator('span')).toContainText('Dashboard');
    await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
-
+  await page.getByRole('link', { name: 'danielcdoran/zoots' }).click();
+  await page.getByRole('link', { name: 'README', exact: true }).click();
+  await page.getByRole('link', { name: 'README', exact: true }).click();
+  await page.getByRole('link', { name: 'README.md, (File)' }).click();
+  await page.getByTestId('edit-button').click();
+  await page.getByRole('textbox', { name: 'Editing README.md file' }).fill('add moreaddd here');
+  await page.getByRole('button', { name: 'Commit changes...' }).click();
+  await page.getByRole('textbox', { name: 'Add an optional extended' }).click();
+  await page.getByRole('textbox', { name: 'Add an optional extended' }).fill('text added');
+  await page.getByRole('button', { name: 'Commit changes', exact: true }).click();
 });
 
     
